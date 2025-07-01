dbd284586dc2e954d7d9a58ff56be9b68851d7d1
Oluchi Akanno <olynelly96@gmail.com>
2025-07-01 13:11:56 +0100

    trying to deploy again

--- app/api/survey/route.js
+++ app/api/survey/route.js
@@ -40,6 +40,7 @@ export async function POST(req) {
 
 export async function GET() {
   await connectToDB()
+  console.log("hello")
 
   try {
     const surveys = await Survey.find().sort({ createdAt: -1 })
